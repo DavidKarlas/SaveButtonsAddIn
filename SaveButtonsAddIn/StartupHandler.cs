7cff0bd9eadfa5c02f5a7d21c5b139e13e50d860
David Karlaš <david.karlas@xamarin.com>
2015-12-14 07:49:42 +0100

    Added MyCommandsBar items and StartupHandler which calls Show("MyCommandsBar")

---
+++ SaveButtonsAddIn/StartupHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+
+namespace SaveButtonsAddIn
+{
+	public class StartupHandler : CommandHandler
+	{
+		protected override void Run()
+		{
+			IdeApp.Workbench.ShowCommandBar ("MyCommandsBar");
+		}
+	}
+}
+
