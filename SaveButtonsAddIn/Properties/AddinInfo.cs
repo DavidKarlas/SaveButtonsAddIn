2d015d908046ae47c1497857757ca3adc3f49466
David Karlaš <david.karlas@xamarin.com>
2015-12-14 07:40:26 +0100

    Initial commit(AddIn template files)

---
+++ SaveButtonsAddIn/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+	"SaveButtonsAddIn",
+	Namespace = "SaveButtonsAddIn",
+	Version = "1.0"
+)]
+
+[assembly: AddinName("SaveButtonsAddIn")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("SaveButtonsAddIn")]
+[assembly: AddinAuthor("David Karlaš")]
+
